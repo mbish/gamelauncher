084973d085206e153317c217acd455a90dc99cde
Mark Bishop <mbishop@duosecurity.com>
2023-03-13 17:16:02 -0400

    Further cleanup

--- src/System.hs
+++ src/System.hs
@@ -1,23 +1,26 @@
-{-# LANGUAGE StrictData, DeriveGeneric #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE StrictData #-}
+
 module System where
-import Data.Aeson
-import Data.Text
+
 import Control.Applicative
 import Control.Monad
+import Data.Aeson
 import qualified Data.ByteString.Lazy as B
+import Data.Text
 import GHC.Generics
 import GameOverrides
 
-
 -- | Type of each JSON entry in record syntax.
-data System  =
-  System { system :: !Text
-         , command  :: !Text
-         , overrides   :: [GameOverride]
-           } deriving (Show,Generic)
+data System = System
+  { system :: !Text,
+    command :: !Text,
+    overrides :: [GameOverride]
+  }
+  deriving (Show, Generic)
 
 -- Instances to convert our type to/from JSON.
 
 instance FromJSON System
-instance ToJSON System
 
+instance ToJSON System
--- src/gamelauncher.hs
+++ src/gamelauncher.hs
@@ -1,25 +1,26 @@
-{-# LANGUAGE OverloadedStrings, DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
 
-import qualified System.Process as Process
-import System.FilePath
-import qualified Data.Aeson as J
-import Data.Text
-import qualified Data.Text.Lazy as LazyText
-import Text.Replace as Replace
-import qualified Data.List as List
 import Control.Applicative
 import Control.Monad
+import qualified Data.Aeson as J
 import qualified Data.ByteString.Lazy as B
+import qualified Data.List as List
+import Data.Text
+import qualified Data.Text.Lazy as LazyText
 import GHC.Generics
-import qualified System as SystemData
-import qualified GameOverrides as GameOverrides
+import qualified GameOverrides
 import Options.Applicative
+import qualified System as SystemData
+import System.FilePath
+import qualified System.Process as Process
+import Text.Replace as Replace
 
-data Options = Options {
-    configFile :: FilePath
-  , gameFile :: FilePath
-  , system :: Text
-} deriving(Show)
+data Options = Options
+  { configFile :: FilePath,
+    gameFile :: FilePath,
+    system :: Text
+  }
+  deriving (Show)
 
 configFileParser :: Parser FilePath
 configFileParser = strOption (long "inputFile" <> short 'i' <> help "Input file " <> metavar "FILE")
@@ -38,49 +39,58 @@ getJSON :: FilePath -> IO B.ByteString
 getJSON = B.readFile
 
 expandCommand :: FilePath -> Text -> Text
-expandCommand gamePath command = LazyText.toStrict $ Replace.replaceWithList [
-        Replace.Replace "{file.path}" (pack gamePath)
-        , Replace.Replace "{file.name}" (pack $ takeFileName gamePath)
-        , Replace.Replace "{file.basename}" (pack $ takeBaseName gamePath)
-        , Replace.Replace "{file.dir}" (pack $ takeDirectory gamePath)
-        , Replace.Replace "{file.uri}" (pack (
-            case isAbsolute gamePath of
-                True -> "file://" ++ gamePath
-                False -> "file:/" ++ gamePath
-        ))
-    ] (LazyText.fromStrict command)
+expandCommand gamePath command =
+  LazyText.toStrict $
+    Replace.replaceWithList
+      [ Replace.Replace "{file.path}" (pack gamePath),
+        Replace.Replace "{file.name}" (pack $ takeFileName gamePath),
+        Replace.Replace "{file.basename}" (pack $ takeBaseName gamePath),
+        Replace.Replace "{file.dir}" (pack $ takeDirectory gamePath),
+        Replace.Replace
+          "{file.uri}"
+          ( pack
+              ( if isAbsolute gamePath
+                  then "file://" ++ gamePath
+                  else "file:/" ++ gamePath
+              )
+          )
+      ]
+      (LazyText.fromStrict command)
 
 findCommand :: FilePath -> Text -> [SystemData.System] -> Maybe Text
 findCommand gamePath systemName config =
-  do {
+  do
     system <- List.find matchingSystem config
-  ; game <- List.find matchingGame (SystemData.overrides system)
-  ; command <- return (GameOverrides.command game)
-  ; return (expandCommand gamePath command)
-  }
+    game <- List.find matchingGame (SystemData.overrides system)
+    let command = GameOverrides.command game
+    return (expandCommand gamePath command)
   where
     matchingSystem :: SystemData.System -> Bool
-    matchingSystem c = (SystemData.system c) == systemName
+    matchingSystem c = SystemData.system c == systemName
 
     matchingGame :: GameOverrides.GameOverride -> Bool
-    matchingGame g = (GameOverrides.name g) == pack (takeBaseName gamePath)
+    matchingGame g = GameOverrides.name g == pack (takeBaseName gamePath)
 
 main :: IO ()
 main = do
- -- Get JSON data and decode it
- opts <- execParser opts
- d <- (J.eitherDecode <$> (getJSON (configFile opts))) :: IO (Either String [SystemData.System])
- -- If d is Left, the JSON was malformed.
- -- In that case, we report the error.
- -- Otherwise, we perform the operation of
- -- our choice. In this case, just print it.
- case d of
-  Left err -> putStrLn err
-  Right ps ->
-    case findCommand (gameFile opts) (system opts) ps of
+  -- Get JSON data and decode it
+  opts <- execParser opts
+  d <- J.eitherDecode <$> getJSON (configFile opts) :: IO (Either String [SystemData.System])
+  -- If d is Left, the JSON was malformed.
+  -- In that case, we report the error.
+  -- Otherwise, we perform the operation of
+  -- our choice. In this case, just print it.
+  case d of
+    Left err -> putStrLn err
+    Right ps ->
+      case findCommand (gameFile opts) (system opts) ps of
         Nothing -> return ()
         Just a -> Process.callCommand (unpack a)
- where
-    opts = info (optionsParser <**> helper)  ( fullDesc
-     <> progDesc "Command launcher based on game and system"
-     <> header "GameLauncher" )
+  where
+    opts =
+      info
+        (optionsParser <**> helper)
+        ( fullDesc
+            <> progDesc "Command launcher based on game and system"
+            <> header "GameLauncher"
+        )
