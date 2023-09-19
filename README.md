1eb48d0123c0ab04af5fb85b0175e3f54e9bcec0
Mark Bishop <mbishop@duosecurity.com>
2023-09-19 14:47:33 -0400

    New build files

---
+++ README.md
@@ -0,0 +1,5 @@
+# gamelauncher
+[![gamelauncher on hackage](https://img.shields.io/hackage/v/gamelauncher)](http://hackage.haskell.org/package/gamelauncher)
+[![gamelauncher on Stackage Nightly](https://stackage.org/package/gamelauncher/badge/nightly)](https://stackage.org/nightly/package/gamelauncher)
+
+Generated with [template-haskell](https://github.com/jonascarpay/template-haskell)
---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "gamelauncher";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-23.05";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = inputs:
+    let
+      overlay = final: prev: {
+        haskell = prev.haskell // {
+          packageOverrides = hfinal: hprev:
+            prev.haskell.packageOverrides hfinal hprev // {
+              gamelauncher = hfinal.callCabal2nix "gamelauncher" ./. { };
+            };
+        };
+        gamelauncher = final.haskell.lib.compose.justStaticExecutables final.haskellPackages.gamelauncher;
+      };
+      perSystem = system:
+        let
+          pkgs = import inputs.nixpkgs { inherit system; overlays = [ overlay ]; };
+          hspkgs = pkgs.haskellPackages;
+        in
+        {
+          devShell = hspkgs.shellFor {
+            withHoogle = true;
+            packages = p: [ p.gamelauncher ];
+            buildInputs = [
+              hspkgs.cabal-install
+              hspkgs.haskell-language-server
+              hspkgs.hlint
+              hspkgs.ormolu
+              pkgs.bashInteractive
+            ];
+          };
+          defaultPackage = pkgs.gamelauncher;
+        };
+    in
+    { inherit overlay; } // inputs.flake-utils.lib.eachDefaultSystem perSystem;
+}
