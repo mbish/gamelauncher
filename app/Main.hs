cbbfd882f564e3c3645884c4d08059bee30bc410
Mark Bishop <mbishop@duosecurity.com>
2026-05-03 13:15:07 -0400

    try sending both

--- app/Main.hs
+++ app/Main.hs
@@ -138,6 +138,7 @@ run hup a = do
           Just pid -> do
             pgid <- getProcessGroupIDOf pid
             signalProcessGroup sigCONT pgid
+            signalProcessGroup sigQUIT pgid
             signalProcessGroup sigTERM pgid
         _ <- Process.waitForProcess ph
         pure ()
